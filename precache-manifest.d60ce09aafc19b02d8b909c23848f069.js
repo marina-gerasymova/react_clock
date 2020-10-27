789513e59f46a6c69f93e5820152375a19bc10b0
Marina <m.gerasymova29@gmail.com>
2020-10-27 12:45:41 +0200

    Updates

---
+++ precache-manifest.d60ce09aafc19b02d8b909c23848f069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c6df52f27eebd759ac8d9f42dca350",
+    "url": "./index.html"
+  },
+  {
+    "revision": "efa3004cf79523177d05",
+    "url": "./static/css/main.39876b74.chunk.css"
+  },
+  {
+    "revision": "f0241297499fcda3d3ea",
+    "url": "./static/js/2.73ed1cec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.73ed1cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa3004cf79523177d05",
+    "url": "./static/js/main.b5c96fb1.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
